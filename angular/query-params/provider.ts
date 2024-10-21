81da2527ae1b72bd9369c04546998960ece5adae
Kenneth <kennethmead784@gmail.com>
2024-10-21 17:08:17 +0000

    updated query parameter store to use enum type

--- angular/query-params/provider.ts
+++ angular/query-params/provider.ts
@@ -1,17 +1,25 @@
 import { InjectionToken, makeEnvironmentProviders, Provider } from "@angular/core";
-import { QueryParamKeys, UntypedQueryParamKeys } from "./query-param-keys";
+import { UntypedQueryParamKeys } from "./query-param-keys";
 import { QueryParamsStore } from "./query-params.store";
 import { provideRouter } from '@angular/router';
 
 export const QUERY_PARAM_KEYS = new InjectionToken<UntypedQueryParamKeys>("QUERY_PARAM_KEYS");
 
 /**
- * Co-requisite provider: {@link provideRouter}
+ * TODO: figure out a way to type this so that it forces the generic type to be an enum with string values
  * 
- * Sets up providers necessary to have helper classes decode query parameters.  
+ * ---
+ * 
+ * {@link provideRouter} must be used in the application configuration for this to work.
+ * 
+ * ---
+ * 
+ * Provides 
+ * - {@link QueryParamsStore} service to interact with url query parameters as signals.
+ * - {@link QUERY_PARAM_KEYS} An enum that maps its keys to the expected query parameter names.
  * @param queryParamKeys An object whos keys map to expected query parameter names
  */
-export function provideQueryParams<T>(queryParamKeys: QueryParamKeys<T>) {
+export function provideQueryParams<TQueryParamsEnum>(queryParamKeys: TQueryParamsEnum) {
   const providers: Provider[] = [
     { provide: QUERY_PARAM_KEYS, useValue: queryParamKeys },
     QueryParamsStore
--- angular/query-params/query-params.store.ts
+++ angular/query-params/query-params.store.ts
@@ -1,38 +1,96 @@
-import { Inject, Injectable } from '@angular/core';
-import { ActivatedRoute, ParamMap, Params, QueryParamsHandling, Router } from '@angular/router';
+import { Inject, Injectable, signal, Signal, WritableSignal } from '@angular/core';
+import { ActivatedRoute, ParamMap, Params, QueryParamsHandling, Router, provideRouter } from '@angular/router';
 import { map, Observable } from 'rxjs';
-import { QueryParamKeys } from './query-param-keys';
 import { QUERY_PARAM_KEYS } from './provider';
+import { UntypedQueryParamKeys } from './query-param-keys';
 
+/**
+ * A service that provides an abstraction layer to interact with url query parameters as signals. 
+ * To subscribe to query parameter changes like an event listener, use the `queryParamMap` property on {@link Router}.
+ * 
+ * Requires 
+ * - Angulars {@link Router} service to be provided. see {@link provideRouter}
+ * - An enum {@link TQueryParamEnum} that maps its keys to the expected query parameter names.
+ * 
+ * @see {@link provideRouter}
+ * @see {@link QueryParamKeys}
+ */
 @Injectable({
   providedIn: 'root'
 })
-export class QueryParamsStore<T> {
-  readonly queryParams = new Map<keyof T, Observable<string | null>>();
+export class QueryParamsStore<TQueryParamEnum extends string> {
+  observables = new Map<TQueryParamEnum, Observable<string[] | null>>();
+  private readonly queryParams_ = new Map<TQueryParamEnum, WritableSignal<string[] | null>>();
+  /**
+   * A map of all query parameters that are being tracked by the store as defined in the {@link TQueryParamEnum} enumerator.
+   * Automatically updates when the query parameter changes using {@link Router}
+   */
+  get queryParams(): Map<TQueryParamEnum, Signal<string[] | null>> {
+    return this.queryParams_;
+  }
 
   constructor(
-    @Inject(QUERY_PARAM_KEYS) public queryParamKeys: QueryParamKeys<T>,
+    @Inject(QUERY_PARAM_KEYS) private queryParamKeys: UntypedQueryParamKeys,
     private router: Router,
     private route: ActivatedRoute
   ) {
-    for (const key in queryParamKeys) {
-      this.queryParams.set(key, this.createQueryParamObservable(key))
+    this.route.queryParamMap.subscribe(this.subscribeToQueryParamChanges.bind(this));
+  }
+
+  /**
+   * Creates observables for all query parameters defined in the {@link TQueryParamEnum} enumerator and stores them in the {@link observables} property
+   */
+  initializeQueryParamObservables() {
+    for (const key in this.queryParamKeys) {
+      this.observables.set(key as TQueryParamEnum, this.getObservable(key as TQueryParamEnum));
     }
   }
 
-  createQueryParamObservable(name: string): Observable<string | null> {
-    return this.route.queryParamMap.pipe(map<ParamMap, string | null>(params => params.get(name)));
+  /**
+   * Creates a shortcut from `this.route.queryParamMap` to the desired query parameter
+   */
+  private getObservable(name: TQueryParamEnum): Observable<string[] | null> {
+    return this.route.queryParamMap.pipe(map<ParamMap, string[] | null>(params => params.getAll(name)));
   }
 
-  update<T>(queryParamKey: keyof QueryParamKeys<T>, value: T, queryParamsHandling: QueryParamsHandling = 'merge') {
-    if (value == null) {
-      return;
+  /**
+   * Subscribes to query parameter changes and updates the store accordingly
+   */
+  private subscribeToQueryParamChanges(params: ParamMap) {
+    params.keys.forEach((key) => {
+      var keyIsNotInEnum = Object.values(this.queryParamKeys).includes(key);
+      if (keyIsNotInEnum) {
+        throw new Error(`Query parameter key '${key}' is not defined in the provided enum`);
+      }
+      var paramValues = params.getAll(key);
+      var typedKey = key as TQueryParamEnum;
+      if (this.queryParams_.has(typedKey) == false) {
+        this.queryParams_.set(typedKey, signal(paramValues));
+        return;
+      }
+      var paramsSignal = this.queryParams_.get(typedKey)
+      if (paramsSignal !== undefined) {
+        paramsSignal.set(paramValues);
+      }
+    })
+  }
+
+  /**
+   * Sets the query parameter to the provided value. If the value is null, the query parameter will be removed
+   * @param name The query parameter key to update
+   * @param value The value to update the query parameter to
+   * @param queryParamsHandling The strategy for handling the query parameters
+   * @returns 
+   */
+  set<T>(name: TQueryParamEnum, value: T, queryParamsHandling: QueryParamsHandling = 'merge') {
+    if (value === undefined) {
+      value = null as T;
     }
-    var currentValue = this.route.snapshot.queryParamMap.get(queryParamKey.toString());
-    const queryParams: Params = { [queryParamKey]: value.toString() };
+    var currentValue = this.route.snapshot.queryParamMap.get(name);
     if (currentValue == value) {
       return;
     }
+    const queryParams: Params = { [name]: value === null ? null : value!.toString() };
     this.router.navigate(
       [],
       {
