edcfe917f089b4e24bfd30b62ee18ca572d67061
Kenneth Mead <48263885+Xerner@users.noreply.github.com>
2024-11-15 01:31:45 -0500

    added fill gaps in array func

--- angular/interceptors/auth/token.ts
+++ angular/interceptors/auth/token.ts
@@ -1,10 +1,4 @@
 import { InjectionToken } from "@angular/core";
-<<<<<<< HEAD
 import { ITokenService } from "./ITokenService";
 
 export const TOKEN_SERVICE = new InjectionToken<ITokenService>("BEARER_TOKEN_SERVICE_TOKEN");
-=======
-import { IBearerTokenProvider } from "./IBearerTokenProvider";
-
-export const BEARER_TOKEN_SERVICE_TOKEN = new InjectionToken<IBearerTokenProvider>("BEARER_TOKEN_SERVICE_TOKEN");
->>>>>>> 7270e2009fe1fda14325cc0e8b29eabef07ec8ac
--- library/arrays.ts
+++ library/arrays.ts
@@ -1,5 +1,7 @@
 import { MATH } from './math'
 
+export type ComparerFunction<T> = (x1: T, x2: T) => number;
+
 export class ArrayService {
   /**
      *
@@ -14,6 +16,33 @@ export class ArrayService {
 
   randomArrayItem<T>(array: T[]) {
     return array[MATH.randomInt(array.length)];
+  }
+
+  fillSparseArray<T>(array: T[], length: number, fillValue: T) {
+    return Array.from({ length }, (_, i) => array[i] ?? fillValue);
+  }
+
+  /**
+   * Sorts the array in ascending order and ensures every item is contiguous without having a value of more than 1 difference between two entries. Does not sort the provided array in place
+   * @param array The source array
+   * @param comparer A function that compares two values and returns the difference between. Used to sort the array and determine the gap between two values
+   * @param modifier A function that takes a value and returns a new value that is contiguous to the previous value
+   * @returns A new sorted array with contiguous values
+   */
+  sortAndFillGaps<T>(array: T[], comparer: ComparerFunction<T>, modifier: (x: T) => T): T[] {
+    if (array.length === 0) return [];
+    var sortedArray = [...array].sort(comparer);
+    const result = [array[0]];
+    for (let i = 0; i < sortedArray.length - 1; i++) {
+      const prev = array[i];
+      const current = array[i + 1];
+      var diff = comparer(current, prev);
+      for (let i = 1; i < diff; i++) {
+        result.push(modifier(prev));
+      }
+      result.push(current);
+    }
+    return result;
   }
 }
 
